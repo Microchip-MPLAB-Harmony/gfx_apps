0efd3354a34239718461f499369af0b02be34b8f
Ed Gallofin <ed.gallofin@microchip.com>
2019-01-30 10:35:22 -0700

    Regenerate aria_bm_e70_xu_tm4301b

--- apps/aria_benchmark/firmware/src/arm/toolchain_specifics.h
+++ apps/aria_benchmark/firmware/src/arm/toolchain_specifics.h
@@ -24,17 +24,10 @@
 #ifndef TOOLCHAIN_SPECIFICS_H
 #define TOOLCHAIN_SPECIFICS_H
 
-#ifdef __ICCARM__
-#define ssize_t long
-#define COMPILER_PRAGMA(arg)            _Pragma(#arg)
-#define SECTION(a) COMPILER_PRAGMA(location = a)
-#define NO_INIT __no_init
-
-#else
 #include <sys/types.h>
-#define NO_INIT __attribute__((section(".no_init")))
+#define NO_INIT    __attribute__((section(".no_init")))
 #define SECTION(a) __attribute__((__section__(a)))
-#endif
 
 
-#endif //TOOLCHAIN_SPECIFICS_H
+#endif // end of header
+
