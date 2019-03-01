5f42444225a42c4937fb63c7cc2cb5ab7ff329e8
Ed Gallofin <ed.gallofin@microchip.com>
2019-03-01 12:33:56 -0700

    aria_showcase - regenerate to tip
    core - v3.2.0-rc.2
    csp - v3.2.0-rc.2
    bsp - v3.2.0-rc.1
    dev_packs - v3.2.0-rc.1
    mhc - v3.2.0-rc.3
    mplabx_plugin - v3.2.0-rc.3

--- apps/aria_showcase/firmware/src/config/lcc_rgb565_mxt/gfx/driver/controller/lcc/drv_gfx_lcc.c
+++ apps/aria_showcase/firmware/src/config/lcc_rgb565_mxt/gfx/driver/controller/lcc/drv_gfx_lcc.c
@@ -339,7 +339,7 @@ static void DRV_GFX_LCC_DisplayRefresh(void)
         {
             if (hSyncs > vsyncPulseDown)
             {
-                GFX_DISP_INTF_PIN_VSYNC_Clear();
+                GFX_DISP_INTF_PIN_VSYNC_Set();
 
                 vsyncPulseUp = hSyncs + DISP_VER_PULSE_WIDTH;
                 vsyncState = VSYNC_PULSE;
@@ -359,7 +359,7 @@ static void DRV_GFX_LCC_DisplayRefresh(void)
         {
             if (hSyncs >= vsyncPulseUp)
             {
-                GFX_DISP_INTF_PIN_VSYNC_Set();
+                GFX_DISP_INTF_PIN_VSYNC_Clear();
                 vsyncEnd = hSyncs + DISP_VER_BACK_PORCH;
                 vsyncState = VSYNC_BACK_PORCH;
             }
@@ -395,7 +395,7 @@ static void DRV_GFX_LCC_DisplayRefresh(void)
         }
         case HSYNC_PULSE:
         {
-            GFX_DISP_INTF_PIN_HSYNC_Clear();
+            GFX_DISP_INTF_PIN_HSYNC_Set();
 
             if (hSyncs >= vsyncPeriod)
             {
@@ -413,7 +413,7 @@ static void DRV_GFX_LCC_DisplayRefresh(void)
         }
         case HSYNC_BACK_PORCH:
         {
-            GFX_DISP_INTF_PIN_HSYNC_Set();
+            GFX_DISP_INTF_PIN_HSYNC_Clear();
 
             hsyncState = HSYNC_DATA_ENABLE; 
 
