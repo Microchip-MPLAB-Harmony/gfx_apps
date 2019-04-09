ded8c97447649fc35f1342213d904a084043989f
Ed Gallofin <ed.gallofin@microchip.com>
2019-04-09 12:16:10 -0700

    MH3-9413 - Add initial version of aria_benchmark for PIC32MZ DA.

---
+++ apps/aria_benchmark/firmware/src/config/glcd_rgba8888_mxt_mzdas_sk/gfx/libaria/libaria_events.h
@@ -0,0 +1,143 @@
+/*******************************************************************************
+  MPLAB Harmony Graphics Composer Generated Definitions Header
+
+  File Name:
+    libaria_events.h
+
+  Summary:
+    Build-time generated definitions header based on output by the MPLAB Harmony
+    Graphics Composer.
+
+  Description:
+    Build-time generated definitions header based on output by the MPLAB Harmony
+    Graphics Composer.
+
+    Created with MPLAB Harmony Version 3.0
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#ifndef _LIBARIA_EVENTS_H
+#define _LIBARIA_EVENTS_H
+
+#include "gfx/libaria/libaria.h"
+#include "gfx/libaria/libaria_init.h"
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END 
+
+// Generated Event Handler - Origin: CounterSizeDownButton, Event: PressedEvent
+void CounterSizeDownButton_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: CounterSizeUpButton, Event: PressedEvent
+void CounterSizeUpButton_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: NextButton, Event: PressedEvent
+void NextButton_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: ButtonWidget1, Event: PressedEvent
+void ButtonWidget1_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: ButtonWidget1, Event: ReleasedEvent
+void ButtonWidget1_ReleasedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: FPSButtonCounter, Event: PressedEvent
+void FPSButtonCounter_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: FPSButtonCounter, Event: ReleasedEvent
+void FPSButtonCounter_ReleasedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: MotionMenuNextButton, Event: PressedEvent
+void MotionMenuNextButton_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: MotionMenuPlusButton, Event: PressedEvent
+void MotionMenuPlusButton_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: MotionMenuMinusButton, Event: PressedEvent
+void MotionMenuMinusButton_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: RectSizeDownButtonWidget, Event: PressedEvent
+void RectSizeDownButtonWidget_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: RectSizeUpButtonWidget, Event: PressedEvent
+void RectSizeUpButtonWidget_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: ButtonWidget2, Event: PressedEvent
+void ButtonWidget2_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: ButtonWidget2, Event: ReleasedEvent
+void ButtonWidget2_ReleasedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: FPSButtonMotion, Event: PressedEvent
+void FPSButtonMotion_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: FPSButtonMotion, Event: ReleasedEvent
+void FPSButtonMotion_ReleasedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: ImageNextButtonWidget, Event: PressedEvent
+void ImageNextButtonWidget_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: ImageSizeDownButtonWidget, Event: PressedEvent
+void ImageSizeDownButtonWidget_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: ImageSizeUpButtonWidget, Event: PressedEvent
+void ImageSizeUpButtonWidget_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: ImageTypePrevButtonWidget, Event: PressedEvent
+void ImageTypePrevButtonWidget_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: ImageTypeNextButtonWidget, Event: PressedEvent
+void ImageTypeNextButtonWidget_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: ButtonWidget3, Event: PressedEvent
+void ButtonWidget3_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: ButtonWidget3, Event: ReleasedEvent
+void ButtonWidget3_ReleasedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: FPSButtonImages, Event: PressedEvent
+void FPSButtonImages_PressedEvent(laButtonWidget* btn);
+
+// Generated Event Handler - Origin: FPSButtonImages, Event: ReleasedEvent
+void FPSButtonImages_ReleasedEvent(laButtonWidget* btn);
+
+
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+#endif // _LIBARIA_EVENTS_H
+/*******************************************************************************
+ End of File
+*/
