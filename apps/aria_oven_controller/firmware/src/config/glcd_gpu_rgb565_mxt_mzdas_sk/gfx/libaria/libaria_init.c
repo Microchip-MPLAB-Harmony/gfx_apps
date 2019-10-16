fe8a5e465979a426e52d056d7da0dec5e99fa83c
Keith Jones <keith.jones@microchip.com>
2019-10-16 15:25:20 -0700

    $ git diff apps/aria_oven_controller/firmware/src/config/glcd_gpu_rgb565_mxt_mzdas_sk/gfx/libaria/libaria_init.c @@ -281,7 +281,7 @@ int32_t libaria_initialize(void)      screen = laScreen_New(LA_FALSE, LA_FALSE, &ScreenCreate_assetLayout);      laContext_AddScreen(screen);
    -    laContext_SetActiveScreen(3);
    +    laContext_SetActiveScreen(0);

--- apps/aria_oven_controller/firmware/src/config/glcd_gpu_rgb565_mxt_mzdas_sk/gfx/libaria/libaria_init.c
+++ apps/aria_oven_controller/firmware/src/config/glcd_gpu_rgb565_mxt_mzdas_sk/gfx/libaria/libaria_init.c
@@ -281,7 +281,7 @@ int32_t libaria_initialize(void)
     screen = laScreen_New(LA_FALSE, LA_FALSE, &ScreenCreate_assetLayout);
     laContext_AddScreen(screen);
 
-    laContext_SetActiveScreen(3);
+    laContext_SetActiveScreen(0);
 
 	return 0;
 }
